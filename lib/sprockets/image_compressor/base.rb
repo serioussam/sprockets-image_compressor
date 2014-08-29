b9fd7d33c25f34481a975565d68fb065f5d5efca
Peter Arentsen <peter.arentsen@holder.nl>
2014-08-29 15:32:46 +0200

    Add check

--- lib/sprockets/image_compressor/base.rb
+++ lib/sprockets/image_compressor/base.rb
@@ -5,6 +5,7 @@ module Sprockets
     GEM_ROOT = File.expand_path File.join(File.dirname(__FILE__), "../../../")
 
     class Base
+      binding.pry
       def binary_path
         @binary_path ||= begin
           try_system_binary or try_vendored_binaries or raise """
@@ -25,6 +26,7 @@ module Sprockets
         # use the first vendored binary that doesn't shit the bed when we ask for its version
         vendored_binaries = Dir["#{GEM_ROOT}/bin/#{@name}.*"].sort
         vendored_binaries.find do |path|
+          binding.pry
           system("#{path} -version 2> /dev/null > /dev/null")
         end
       end
