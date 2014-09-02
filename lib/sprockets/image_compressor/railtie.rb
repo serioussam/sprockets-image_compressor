0d7a6cfde9bb4013901fb0ff1abfa7d9cc3ea6a1
Peeyush Aggarwal <peeyush@name-coach.com>
2014-09-02 21:26:28 +0000

    Fixed group for installing railties

--- lib/sprockets/image_compressor/railtie.rb
+++ lib/sprockets/image_compressor/railtie.rb
@@ -1,7 +1,7 @@
 module Sprockets
   module ImageCompressor
     class Railtie < ::Rails.version < '4.0' ? ::Rails::Engine : ::Rails::Railtie
-      initializer :setup_image_compressors do |app|
+      initializer :setup_image_compressors, :group => :all do |app|
         Integration.setup app.assets if app.config.assets.compress
       end
     end
